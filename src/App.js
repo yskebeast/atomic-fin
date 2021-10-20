36e4e577194448d49cbc778a7647d4b0d62fd387
yskebeast <84611106+yskebeast@users.noreply.github.com>
2021-10-20 16:53:56 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+// import { BrowserRouter } from "react-router-dom";
+// import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+// import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+// import { SearchInput } from "./components/molecules/SearchInput";
+// import { UserCard } from "./components/organisms/user/UserCard";
+// import { DefaultLayout } from "./components/templates/DefaultLayout";
+// import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { UserProvider } from "./porviders/UserProvider";
 import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
-  return <Router />;
+  return (
+    <UserProvider>
+      <Router />
+    </UserProvider>
+  );
 }
